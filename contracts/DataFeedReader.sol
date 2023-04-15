6cdc4ec837ef71d4830c722c749ba792e85269da
Vansh Wassan <007blackhacker@gmail.com>
2023-04-15 07:10:05 +0530

    First commit

---
+++ contracts/DataFeedReader.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@api3/contracts/v0.8/interfaces/IProxy.sol";
+
+contract DataFeedReaderExample is Ownable {
+    // This contract reads from a single proxy. Your contract can read from multiple proxies.
+    address public proxy;
+
+    // Updating the proxy address is a security-critical action. In this example, only
+    // the owner is allowed to do so.
+    function setProxy(address _proxy) public onlyOwner {
+        proxy = _proxy;
+    }
+
+    function readDataFeed()
+        external
+        view
+        returns (int224 value, uint256 timestamp)
+    {
+        (value, timestamp) = IProxy(proxy).read();
+        // If you have any assumptions about `value` and `timestamp`, make sure
+        // to validate them right after reading from the proxy.
+    }
+}
---
+++ deploy/deploy.ts
@@ -0,0 +1,52 @@
+import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+require('dotenv').config()
+
+// An example of a deploy script that will deploy and call the DataFeedReader Contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the DataFeedReader contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet(process.env.PRIVATE_KEY);
+
+  // Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("DataFeedReaderExample");
+
+  // Estimate contract deployment fee
+  // const greeting = "Hi there!";
+  const deploymentFee = await deployer.estimateDeployFee(artifact, []);
+
+  // // OPTIONAL: Deposit funds to L2
+  // // Comment this block if you already have funds on zkSync.
+  // const depositHandle = await deployer.zkWallet.deposit({
+  //   to: deployer.zkWallet.address,
+  //   token: utils.ETH_ADDRESS,
+  //   amount: deploymentFee.mul(2),
+  // });
+  // // Wait until the deposit is processed on zkSync
+  // await depositHandle.wait();
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+
+  const DataFeedReaderContract = await deployer.deploy(artifact, []);
+
+  // //obtain the Constructor Arguments
+  // console.log("constructor args:" + greeterContract.interface.encodeDeploy([]));
+
+  // Show the contract info.
+  const contractAddress = DataFeedReaderContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+
+  // Set the proxy address
+  const setDapiProxy =  await DataFeedReaderContract.setProxy("0x28ce555ee7a3daCdC305951974FcbA59F5BdF09b");
+  await setDapiProxy.wait(5);
+
+  const readDataFeed = await DataFeedReaderContract.readDataFeed();
+  console.log("readDataFeed: " + readDataFeed);
+}
